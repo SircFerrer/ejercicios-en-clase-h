9fd7146c1a050b1375a8de0193e8a7d2987c6b29
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-13 21:01:29 +0100

    Added module 2 lesson 4 exercises

---
+++ module-2/lesson-4/functions-examples/js/main.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+'use strict';
+
+function getCircleAreaWithFunction(radius) {
+  return Math.PI * radius * radius;
+}
+
+console.log(getCircleAreaWithFunction(100) + ' metros cuadrados');
+
+const getCircleAreaWithAnonymousFunction = function(radius) {
+  return Math.PI * radius * radius;
+};
+
+console.log(getCircleAreaWithAnonymousFunction(100) + ' metros cuadrados');
+
+const getCircleAreaWithArrowFunction = radius => Math.PI * radius * radius;
+
+console.log(getCircleAreaWithArrowFunction(100) + ' metros cuadrados');
