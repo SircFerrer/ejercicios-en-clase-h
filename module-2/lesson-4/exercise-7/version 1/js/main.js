0bd1484d900dfaf0aa2982c71b9633365e286cab
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-14 17:19:13 +0100

    Added m2 l5 e7

---
+++ module-2/lesson-4/exercise-7/version 1/js/main.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+'use strict';
+
+function getEl(selector) {
+  return document.querySelector(selector);
+}
+
+function isOdd(selector) {
+  const element = getEl(selector);
+  const number = parseInt(element.innerHTML);
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const oddResult = isOdd('.odd-number');
+console.log(oddResult);
+
+const evenResult = isOdd('.even-number');
+console.log(evenResult);
---
+++ module-2/lesson-4/exercise-7/version 2/js/main.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+'use strict';
+
+function getElContent(selector) {
+  const element = getEl(selector);
+  return element.innerHTML;
+}
+
+function getEl(selector) {
+  return document.querySelector(selector);
+}
+
+function isOdd(selector) {
+  let number = getElContent(selector);
+  number = parseInt(number);
+  return number % 2 === 0;
+}
+
+const oddResult = isOdd('.odd-number');
+console.log(oddResult);
+
+const evenResult = isOdd('.even-number');
+console.log(evenResult);
