79567c040c5149e98b5ae8356e61d59d49e22b1a
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-14 18:13:23 +0100

    Added m2 l5 examples

---
+++ module-2/lesson-5/example-target-vs-currenttarget/js/main.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+'use strict';
+
+// container
+
+const handleContainerClick = ev => {
+  console.log('handleContainerClick: el elemento pulsado es', ev.target);
+  console.log('handleContainerClick: el elemento escuchado es', ev.currentTarget);
+  ev.preventDefault();
+};
+
+const container = document.querySelector('.js-container');
+container.addEventListener('click', handleContainerClick);
+
+// links
+
+const handleLinkClick = ev => {
+  console.log('handleLinkClick: el elemento pulsado es', ev.target);
+  console.log('handleLinkClick: el elemento escuchado es', ev.currentTarget);
+
+  ev.preventDefault(); // des/comentar esta linea a ver qué pasa
+  // ev.stopPropagation(); // des/comentar esta linea a ver qué pasa
+};
+
+const linkAdalab = document.querySelector('.js-link-adalab');
+linkAdalab.addEventListener('click', handleLinkClick);
+
+const linkGoogle = document.querySelector('.js-link-google');
+linkGoogle.addEventListener('click', handleLinkClick);
