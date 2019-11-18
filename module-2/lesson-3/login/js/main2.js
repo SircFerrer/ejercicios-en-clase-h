60c3fe8f28d20e47f1835ddd23baa53b4dd4e521
pixelmary <maria.hojas@gmail.com>
2019-11-18 18:49:28 +0100

    Add small comment

--- module-2/lesson-3/login/js/main2.js
+++ module-2/lesson-3/login/js/main2.js
@@ -1,31 +1,25 @@
 'use strict';
 
+//accedo hasta los elementos que voy a manejar
 const elementUserInput = document.querySelector('#user-name');
 const elementPassInput = document.querySelector('#user-password');
 const elementButton = document.querySelector('#login-button');
 const elementErrorWrapper = document.querySelector('.error-wrapper');
 
+//declaro el valor de mi usuario y contraseña
 const userName = 'pixelmary';
 const userPassword = 'bicicleta';
-let attemps = 0;
 
-// const increaseAttemps = () => attemps < 3 ? attemps++ : console.log('estas bloqueado')
-
-function increaseAttemps() {
-  if(attemps < 3){
-    attemps++
-  }else{
-    console.log('estas bloqueado')
-  }
-}
-
-
+//función de login 
 function login () {
+
+  //¿coinciden los valores de usuario y contraseña con los valores que he declarado?
   if(elementUserInput.value === userName && elementPassInput.value === userPassword) {
+    //SI coinciden, guay, puedo entrar, por si viniera de un error me aseguro que mi error tiene la clase hidden
     elementErrorWrapper.classList.add('hidden');
     alert('Estás dentro');
   } else {
-    increaseAttemps();
+    //EN LOS DEMÁS CASOS, no puedo entrar, algo falla muestro mi párrafo de error quitándole la clase hidden
     elementErrorWrapper.classList.remove('hidden');
   }
 }
