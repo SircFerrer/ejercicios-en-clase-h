441e8035205f3cdefd69ce80f95196f40b8b3fe3
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-24 23:03:40 +0100

    Added module 10 examples

--- module-2/lesson-10/example fetch and local server/js/main.js
+++ module-2/lesson-10/example fetch and local server/js/main.js
@@ -11,11 +11,21 @@ const hideLoading = () => {
   document.querySelector('.js-loading').classList.toggle('hidden', true);
 };
 
+const ensureData = data => {
+  // en esta funcion aseguro los datos, por si hay algún dato del servidor que no viene relleno
+  data.name = data.name || 'Nombre desconocido';
+  data.breed = data.breed || 'Raza desconocida';
+  data.age = data.age || 'Edad desconocida';
+  data.hasChip = data.hasChip || false;
+  data.owner = data.owner || 'Dueña desconocida';
+};
+
 const printData = data => {
   document.querySelector('.js-name').innerHTML = data.name;
   document.querySelector('.js-breed').innerHTML = data.breed;
   document.querySelector('.js-age').innerHTML = data.age;
   document.querySelector('.js-has-chip').innerHTML = data.hasChip;
+  document.querySelector('.js-owner').innerHTML = data.owner;
 };
 
 const printError = err => {
@@ -28,6 +38,7 @@ const getDataFromServer = function(ev) {
   console.log('Pedimos los datos');
   fetch('./server-data/perretes.json')
     .then(response => {
+      hideLoading();
       console.log(
         'El servidor contesta de forma asíncrona con la respuesta',
         response
@@ -40,7 +51,7 @@ const getDataFromServer = function(ev) {
       return data;
     })
     .then(data => {
-      hideLoading();
+      ensureData(data);
       printData(data);
     })
     .catch(err => {
