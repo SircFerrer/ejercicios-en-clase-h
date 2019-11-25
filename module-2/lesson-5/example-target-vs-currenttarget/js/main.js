c6215732887ba87996ab07514cc54fdbac3d52fd
Miguel del Mazo <migueldelmazo@gmail.com>
2019-11-25 18:14:51 +0100

    Added mor info to module 2 lesson 5 example

--- module-2/lesson-5/example-target-vs-currenttarget/js/main.js
+++ module-2/lesson-5/example-target-vs-currenttarget/js/main.js
@@ -8,6 +8,7 @@ const linkGoogle = document.querySelector('.js-link-google');
 // container
 
 const handleContainerClick = ev => {
+  // el elemento pulsado y escuchado NO es el mismo porque NO estamos escuchando el link, sino el container
   console.log('handleContainerClick: el elemento pulsado es', ev.target);
   console.log('handleContainerClick: el elemento escuchado es', ev.currentTarget);
   ev.preventDefault();
@@ -18,6 +19,7 @@ container.addEventListener('click', handleContainerClick);
 // links
 
 const handleLinkClick = ev => {
+  // el elemento pulsado y escuchado es el mismo porque estamos pulsando y escuchando sobre el link
   console.log('handleLinkClick: el elemento pulsado es', ev.target);
   console.log('handleLinkClick: el elemento escuchado es', ev.currentTarget);
 
