dc0c12f6365d251f0436b7e89caff9eb4da0c052
pixelmary <maria.hojas@gmail.com>
2019-12-19 09:12:52 +0100

    Add trivial changes

--- module-3/lesson-6/lifting/src/components/App.js
+++ module-3/lesson-6/lifting/src/components/App.js
@@ -8,7 +8,11 @@ import Header from './Header';
 //dentro de header, la función se la pasaré a través de header por props hasta
 //mi form, form recogerá el valor de mi input y lo mandará a mi componente madre.
 class App extends React.Component {
-
+  constructor(props){
+    super(props);
+    this.consoleValue = this.consoleValue.bind(this);
+  }
+  
   //aquí defino la función que pinta como método de mi componente App
   //recibe algo y pinta ese algo, este algo valdrá lo que sea que le llegué como input.value
   //desde mi hijo
